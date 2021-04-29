a9c804c0c7338f380c28bcfc595ed9396d34e641
Shreya Shandilya <shreya@shandilya.com>
2021-04-29 16:42:07 +0530

    Initial git commit -m Initial

---
+++ todos-list/src/MyComponents/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            Hi,I'm Shreya
+            <hr/>
+            <p>
+                This is a simple website I created using react. It helps me to organize my work by making a todo list.
+            </p>
+        </div>
+    )
+}
---
+++ todos-list/src/MyComponents/Todos.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {TodoItems} from "../MyComponents/TodoItems"
+export const Todos = (props) => {
+    return (
+        <div className="container">
+        <h3 className=" my-3">Todos-List</h3>
+        {props.todos.length===0?"No todos to display":
+        props.todos.map((todo)=>{
+            
+            return(             
+                <TodoItems todo={todo} key={todo.sno} onDelete={props.onDelete}/>
+            )
+        }) } 
+          
+        </div>
+    )
+}
